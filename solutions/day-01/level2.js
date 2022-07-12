57e354512a75c4c944282228cb5fd14b63702612
Ramanujam Gond <ramanujamgond@gmail.com>
2022-07-12 12:05:59 +0530

    Solved the level 2 problems

--- solutions/day-01/level2.js
+++ solutions/day-01/level2.js
@@ -58,3 +58,25 @@ for (let i = 0; i < countryList.length; i++) {
         break;
     }
 }
+
+// In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+let initial = 0;
+while (initial < webTech.length) {
+    if (webTech.indexOf("Sass") != -1) {
+        console.log("Sass is a CSS preprocess");
+        break;
+    } else {
+        webTech.push("Sass");
+        console.log(webTech);
+        break;
+    }
+    initial++;
+}
+
+// Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node', 'Express', 'MongoDB'];
+
+const fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack);
---
+++ solutions/day-01/level3.js
@@ -0,0 +1,44 @@
+// The following is an array of 10 students ages: js const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24] - Sort the array and find the min and max age - Find the median age(one middle item or two middle items divided by two) - Find the average age(all items divided by number of items) - Find the range of the ages(max minus min) - Compare the value of (min - average) and (max - average), use abs() method
+
+// Sort the array and find the min and max age
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+const sortedAges = ages.sort();
+console.log(sortedAges);
+
+const minAge = sortedAges[0];
+const maxAge = sortedAges[sortedAges.length - 1];
+
+// Find the median age(one middle item or two middle items divided by two) 
+console.log("Minimum age is: ", minAge);
+console.log("Maximum age is: ", maxAge);
+
+const mediabAge = sortedAges[sortedAges.length / 2];
+
+console.log(mediabAge);
+
+// Find the average age(all items divided by number of items)
+let total = 0;
+for (let i = 0; i < sortedAges.length; i++) {
+    total = + sortedAges[i];
+}
+
+console.log("Average age: ", Math.floor(total / sortedAges.length));
+
+// Find the range of the ages(max minus min)
+console.log(maxAge - minAge);
+
+// Compare the value of (min - average) and (max - average), use abs() method
+console.log(Math.abs(maxAge - minAge));
+
+// Slice the first ten countries from the countries array
+// import country array list 
+import countryList from "./countries.js";
+
+const updatedCountryList = countryList.slice(0, 10);
+
+console.log(updatedCountryList);
+
+// Find the middle country(ies) in the countries array
+const middleArrayItem = countryList;
+
+console.log(middleArrayItem.length);
