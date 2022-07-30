1e92839d5c4d15110462455e1659ff66b6351517
Bookingjini Labs <36537203+bikidsx@users.noreply.github.com>
2022-07-30 19:31:19 +0530

    Solved all the question for higher order function

--- solutions/day-04/level1.js
+++ solutions/day-04/level1.js
@@ -65,3 +65,127 @@ const filterProductPrice = productsList.filter((item) => {
 });
 
 console.log(filterProductPrice);
+
+// Use method chaining to get the sum of the prices(map, filter, reduce)
+const sumOfPrices = productsList.filter((item) => {
+    if (item.price !== "") {
+        return item.price;
+    }
+}).map((item) => {
+    return item.price;
+}).reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+});
+
+console.log("Sum", sumOfPrices);
+
+// another way to chain the map filter and reduce
+const totalPrice = productsList
+    .map((items) => {
+        return items.price;
+    })
+    .filter((prices) => {
+        if (prices !== "") {
+            return prices;
+        }
+    })
+    .reduce((sum, price) => {
+        return sum + price;
+    });
+
+console.log("Total Price amount is:", totalPrice);
+
+
+// Optimize version of method chaining
+let getAmount = (item) => {
+    if (item.price !== "") {
+        return item.price;
+    }
+};
+
+let priceAmount = (amount) => {
+    return amount.price;
+};
+
+let sumOfAmount = (prevValue, curValue) => {
+    return prevValue + curValue;
+};
+
+const optimizeSum = productsList.filter(getAmount).map(priceAmount).reduce(sumOfAmount);
+
+console.log("Sum of prices is:", optimizeSum);
+
+// Calculate the sum of all the prices using reduce only
+const productsListReduce = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: '' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+]
+
+const sumUsingReduce = productsListReduce.filter((item) => {
+    if (item.price !== "") {
+        return item.price;
+    }
+}).reduce((previousVal, currentVal) => {
+    return previousVal + currentVal.price;
+}, 0);
+
+console.log("Sum is", sumUsingReduce);
+
+// Find the first product which doesn't have a price value
+const noPrice = productsList.find((item) => {
+    if (item.price === "") {
+        return item;
+    }
+});
+
+console.log(noPrice);
+
+// Find the index of the first product which does not have price value
+
+const productNoPrice = productsList.findIndex((item) => {
+    if (item.price === "") {
+        return item;
+    }
+});
+
+console.log(productNoPrice);
+
+// Check if some products do not have a price value
+const someProductNoPrice = productsList.some((item) => {
+    if (item.price === "") {
+        // return console.log("Yes some products do not have prices");
+        return item;
+    }
+})
+
+console.log(someProductNoPrice);
+
+// Check if all the products have price value
+const checkAllPrice = productsList.every((item) => {
+    if (item.price !== "") {
+        return item;
+    }
+})
+
+console.log(checkAllPrice);
+
+// Explain the difference between forEach, map, filter and reduce
+// forEach -> The forEach method is used only with array and we use forEach if you are interested in each item or index or both.
+// map -> We use the map method whenever we like to modify an array. We use the map method only with arrays and it always returns an array.
+// filter -> The filter method like forEach and map is used with an array and it returns an array of the filtered items.
+// reduce -> We use the reduce method to sum all numbers in an array together, or to multiply items in an array or to concatenate items in an array.
+
+
+// Explain the difference between filter, find and findIndex
+// filter -> The filter method like forEach and map is used with an array and it returns an array of the filtered items.
+// find -> find return the first occurrence of an item instead of an array.
+// findIndex -> The findIndex return the index of the first occurrence of an item.
+
+
+// Explain the difference between some and every
+// some -> The some method is used with array and return a boolean. If one or some of the items satisfy the criteria the result will be true else it will be false. Let us see it with an example.
+// every -> The method every is somehow similar to some but every item must satisfy the criteria. The method every like some returns a boolean.
